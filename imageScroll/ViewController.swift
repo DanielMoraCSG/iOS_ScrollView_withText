d5685339499312ae217244b9c5df502eedfab8e8
DanielMoraCSG <daniel@csgroup.mx>
2016-11-07 10:25:05 -0600

    Initial Commit

---
+++ imageScroll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  imageScroll
+//
+//  Created by daniel on 07/11/16.
+//  Copyright © 2016 CS Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
